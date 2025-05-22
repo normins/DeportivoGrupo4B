3b052aebe6b67ffce085c182e02a83b2f2efaacf
gildamorgante <gigimorgante90@gmail.com>
2025-05-22 20:26:08 -0300

    Creacion clase UserDBHelper.kt

---
+++ app/src/main/java/com/club4b/deportivogrupo4b/UserDBHelper.kt
@@ -0,0 +1,35 @@
+package com.club4b.deportivogrupo4b
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class UserDBHelper(context: Context) : SQLiteOpenHelper(context, "UsuariosDB", null, 1) {
+
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL("""
+            CREATE TABLE usuarios (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nombre TEXT UNIQUE,
+                contrasena TEXT
+            )
+        """.trimIndent())
+
+        db.execSQL("INSERT INTO usuarios (nombre, contrasena) VALUES ('admin', '1234')")
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        TODO(   )
+    }
+
+    fun login (nombre: String, contrasena: String): Boolean {
+        val db= readableDatabase
+        val cursor= db.rawQuery(
+            "SELECT * FROM usuarios WHERE nombre=? and contrasena=?",
+            arrayOf(nombre, contrasena)
+        )
+
+        val existe= cursor.count>0
+        return existe
+    }
+}
