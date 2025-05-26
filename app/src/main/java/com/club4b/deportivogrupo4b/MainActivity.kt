8bf0ae6c81dac369e738fe347b31af137838f60b
gildamorgante <gigimorgante90@gmail.com>
2025-05-26 20:48:15 -0300

    Incorporacion de dbHelper a MainActivity

--- app/src/main/java/com/club4b/deportivogrupo4b/MainActivity.kt
+++ app/src/main/java/com/club4b/deportivogrupo4b/MainActivity.kt
@@ -3,6 +3,8 @@ package com.club4b.deportivogrupo4b
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -21,5 +23,29 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
             finish() // evita volver atrás, al login
         }
+
+        val dbHelper = UserDBHelper (this)
+
+        val user = findViewById<EditText>(R.id.etUserName)
+        val pass = findViewById<EditText>(R.id.etPassword)
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+
+        btnLogin.setOnClickListener {
+
+            val userString= user.text.toString().trim()
+            val passString= pass.text.toString().trim()
+
+            if (dbHelper.login(userString,passString)) {
+                Toast.makeText(this, "Bienvenido", Toast.LENGTH_SHORT).show()
+                val intent= Intent (this, MenuPrincipalActivity :: class.java)
+                startActivity(intent)
+            }
+            else{
+                Toast.makeText(this, "Usuario o Contraseña incorrectos", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+
     }
 }
